16ba196992179b7cddb9d435e342ccc98401e6bb
George Thomas <georgesilverbeech@gmail.com>
2022-10-17 20:51:11 +0100

    NB: File imports weren't working because the "resolve alias" part of the webpack was incorrectly configured.
    For future, whenever the webpack aliases are changed, they also need to be changed in tsconfig.json

--- src/routes/addComment.ts
+++ src/routes/addComment.ts
@@ -1,5 +1,5 @@
 import { Request, Response, Router } from "express";
-import {storeMatch} from "../db/store";
+import {storeMatch} from "~/db/store";
 
 interface AddCommentsRequestBody {
   region: string;
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const nodeExternals = require('webpack-node-externals');
 module.exports = env => {
 
   const isDevelopment = env && env.development;
+  const rootDir = path.join(__dirname)
 
   return {
     entry: './src/index.ts',
@@ -23,6 +24,9 @@ module.exports = env => {
     },
     resolve: {
       extensions: ['.ts', '.js'],
+      alias: {
+        "~": path.resolve(rootDir, 'src')
+      }
     },
     output: {
       filename: 'main.js',
